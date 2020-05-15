8ee538e3c76a2ad669b439debfc46ee09f9d8689
Dolphin1986 <56298434+Dolphin1986@users.noreply.github.com>
2020-05-15 17:51:21 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,7 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-
-$bot->run();
-
-$telegram -> useGetUpdatesWithoutDatabase();
-
-$hook_url = __DIR__'/hook.php';
-
-$result = $telegram->getUpdate($hook_url);
-
 echo $result;
-
-// $result = Request::sendMessage([
-//     'chat_id' => $chat_id,
-//     'text'    => 'Your utf8 text 😜 ...',
-// ]);
 
 ?>
 
