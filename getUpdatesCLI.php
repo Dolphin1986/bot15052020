ec354f5ef1821ca25e95aefaed1ae09d8b76c598
Dolphin1986 <56298434+Dolphin1986@users.noreply.github.com>
2020-05-15 17:50:04 +0300

    Create getUpdatesCLI.php

---
+++ getUpdatesCLI.php
@@ -0,0 +1,7 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+$bot_api_key  = '1238595732:AAFgb41Yn2mrvWB27F1HjPbfEyY6P3JBM9I';
+$bot_username = 'My15052020bot';
+
+$telegram->useGetUpdatesWithoutDatabase()
