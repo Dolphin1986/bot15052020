67b84a173ac2ebbe1eab3a611dc553c6033863c6
Dolphin1986 <56298434+Dolphin1986@users.noreply.github.com>
2020-05-15 16:47:47 +0300

    02

--- index.php
+++ index.php
@@ -1,29 +1,4 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
-
-$bot_api_key  = '1238595732:AAFgb41Yn2mrvWB27F1HjPbfEyY6P3JBM9I';
-$bot_username = 'My15052020bot';
-$hook_url     = 'https://bot15052020.herokuapp.com/hook.php';
-
-try {
-    // Create Telegram API object
-    $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
-
-    // Set webhook
-    $result = $telegram->setWebhook($hook_url);
-    if ($result->isOk()) {
-        echo $result->getDescription();
-    }
-} catch (Longman\TelegramBot\Exception\TelegramException $e) {
-    // log telegram errors
-    // echo $e->getMessage();
-}
-
-// $data = file_get_contents(__DIR__.'/log.txt',true);
-
-// echo "<pre>";
-// echo ($data);
-// echo "<pre>";
 
 ?>
 
--- set.php
+++ set.php
@@ -1,18 +1,20 @@
 <?php
-// Load composer
 require __DIR__ . '/vendor/autoload.php';
 
 $bot_api_key  = '1238595732:AAFgb41Yn2mrvWB27F1HjPbfEyY6P3JBM9I';
 $bot_username = 'My15052020bot';
+$hook_url     = 'https://bot15052020.herokuapp.com/hook.php';
 
 try {
     // Create Telegram API object
     $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
 
-    // Handle telegram webhook request
-    $telegram->handle();
+    // Set webhook
+    $result = $telegram->setWebhook($hook_url);
+    if ($result->isOk()) {
+        echo $result->getDescription();
+    }
 } catch (Longman\TelegramBot\Exception\TelegramException $e) {
-    // Silence is golden!
     // log telegram errors
     // echo $e->getMessage();
 }
