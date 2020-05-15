381618ed81015fe44fb016c5cadaa9e4e3df23e6
Dolphin1986 <56298434+Dolphin1986@users.noreply.github.com>
2020-05-15 17:13:52 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,12 @@ $telegram- > useGetUpdatesWithoutDatabase ();
 
 $bot->run();
 
+$bot- > setCustomGetUpdatesCallback (function (ServerResponse $ get_updates_response) {
+    $results = array_filter ((array) $get_updates_response- > getResult ());
+
+    return sprintf ('Есть% d update (s)'. PHP_EOL, count ($results));
+});
+
 $result = Request::sendMessage([
     'chat_id' => $chat_id,
     'text'    => 'Your utf8 text 😜 ...',
