fb0a7493782719e835ecfd89ddd19ebf45ead767
Dolphin1986 <56298434+Dolphin1986@users.noreply.github.com>
2020-05-15 17:21:09 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,9 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-$telegram- > useGetUpdatesWithoutDatabase ();
+$telegram -> useGetUpdatesWithoutDatabase ();
 
 $bot->run();
-
-$bot- > setCustomGetUpdatesCallback (function (ServerResponse $ get_updates_response) {
-    $results = array_filter ((array) $get_updates_response- > getResult ());
-
-    return sprintf ('Есть% d update (s)'. PHP_EOL, count ($results));
-});
 
 $result = Request::sendMessage([
     'chat_id' => $chat_id,
